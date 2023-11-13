c9f5887a34c188f37eef643657006b45630501af
Pavel_Gelfer <9153220501@mail.ru>
2023-11-13 06:36:52 +0300

    1st commit

---
+++ src/main/java/com/CartDemo13/ShopCartD13/ShopCartD13Application.java
@@ -0,0 +1,13 @@
+package com.CartDemo13.ShopCartD13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCartD13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCartD13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/CartDemo13/ShopCartD13/ShopCartD13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CartDemo13.ShopCartD13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCartD13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
